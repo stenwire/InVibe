a160de785bea34c5f5a9daaf37ec5eb116a1b4be
Stephen Nwankwo <nwankwostephen039@yahoo.com>
2022-10-23 11:07:16 +0100

    [chore]: Add model classes

--- musicapp/admin.py
+++ musicapp/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Artiste
+from .models import Song
+from .models import Lyric
+
+admin.site.register(Artiste)
+admin.site.register(Song)
+admin.site.register(Lyric)
--- musicapp/models.py
+++ musicapp/models.py
@@ -1,3 +1,52 @@
+from datetime import datetime
 from django.db import models
 
 # Create your models here.
+
+
+class Artiste(models.Model):
+    """A model containing artiste details
+
+    Attributes:
+        first_name  The artiste first name
+        last_name   The artiste last name
+        age         The artiste age
+    """
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    age = models.IntegerField(default=0)
+
+    def __repr__(self) -> str:
+        return f'<{self.first_name}>'
+
+
+class Song(models.Model):
+    """A model containing Song details
+
+    Attributes:
+        title               The song name
+        date_released       The song release date
+        likes               The song likes
+        artiste_id          The artiste id
+    """
+    title = models.CharField(max_length=200)
+    date_released = models.DateField('date released', default=datetime.now)
+    likes = models.IntegerField(default=0)
+    artiste_id = models.ForeignKey(Artiste, on_delete=models.CASCADE)
+
+    def __repr__(self) -> str:
+        return f'<{self.title}>, <{self.artiste_id}>'
+
+
+class Lyric(models.Model):
+    """A model containing Lyric details
+
+    Attributes:
+        content   The entire song content
+        song_id   The song id
+    """
+    content = models.TextField()
+    song_id = models.ForeignKey(Song, on_delete=models.CASCADE)
+
+    def __repr__(self) -> str:
+        return f'<{self.content}>, <{self.song_id}>'
